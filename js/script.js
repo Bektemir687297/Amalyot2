f1c31b17a685e3db9cf4ac8876598a7b92d6e1fd
Bek <bek.abduazimov@gmail.com>
2022-03-13 18:01:39 +0500

    bek

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-"use strict";
+" use strict";
+
+const btnNews = document.querySelector(".promo__btn"),
+  newsGenre = document.querySelector(".promo__genre"),
+  bg = document.querySelector(".promo__bg"),
+  listNews = document.querySelector(".promo__interactive-list");
 
 const news = [
   "FOOTBALL",
@@ -7,3 +12,18 @@ const news = [
   "BOX",
   "AMERICAN FOOTBAL IN EU...",
 ];
+
+btnNews.remove();
+
+newsGenre.textContent = "uznews";
+newsGenre.style.color = "crimson";
+bg.style.backgroundImage = "url(img/2.jpg)";
+listNews.innerHTML = "";
+news.forEach((itemNews, index) => {
+  listNews.innerHTML += `
+  <li class="promo__interactive-item">
+  ${index + 1}  ${itemNews}
+  <div class="delete"></div>
+</li>
+`;
+});
